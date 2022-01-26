8d20dac8389a5f4920fabb4b2ac571263b609aec
sajalroy2020 <sajal2020roy@gmail.com>
2022-01-26 16:06:10 +0600

    done

---
+++ asset/js/index.js
@@ -0,0 +1,10 @@
+
+
+
+
+let mainMenu = document.getElementById('main_menu');
+let mainBtn = document.getElementById('main_btn');
+
+mainBtn.addEventListener('click', function() {
+   mainMenu.classList.toggle('d-none');
+});
